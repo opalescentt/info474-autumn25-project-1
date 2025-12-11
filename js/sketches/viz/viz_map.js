8cc8249861e25349ef317c0ad8efdd1704bf49b9
phamegan <megpham556@gmail.com>
2025-12-10 22:53:56 -0800

    lowered legend text

--- js/sketches/viz/viz_map.js
+++ js/sketches/viz/viz_map.js
@@ -395,7 +395,7 @@ const worldMapSketch = (p) => {
   
     p.fill(255);
     p.textSize(12);
-    p.text("Longevity (Years Participated)", x + 80, y - 20);
+    p.text("Longevity (Years Participated)", x + 80, y - 8);
   
     let steps = maxYearsParticipated;  
     let blockWidth = w / (steps + 1);
